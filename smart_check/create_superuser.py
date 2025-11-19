d269541afa649d3982a9b1ac33d65ba321e2b7a5
Teerdaveni <teerdaveni@sriainfotech.com>
2025-11-19 17:08:12 +0530

    superuser

---
+++ smart_check/create_superuser.py
@@ -0,0 +1,20 @@
+# create_superuser.py
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smart_check.settings')
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = "admin"
+email = "teerdaveni@sriainfotech.com"
+password = "Admin@123"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print("Superuser created successfully!")
+else:
+    print("Superuser already exists.")
