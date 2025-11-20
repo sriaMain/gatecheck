3a28675a8180e10c48a3b4592fa2d93d2e0f2ff0
Teerdaveni <teerdaveni@sriainfotech.com>
2025-11-20 13:26:56 +0530

    debug false

--- smart_check/settings.py
+++ smart_check/settings.py
@@ -236,13 +236,24 @@ DATABASES = {
 # DEFAULT_FROM_EMAIL = FROM_EMAIL
 
 
-SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
-FROM_EMAIL = os.environ.get("FROM_EMAIL")
+# SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
+# FROM_EMAIL = os.environ.get("FROM_EMAIL")
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.sendgrid.net"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "apikey"        # always "apikey"
-EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
-DEFAULT_FROM_EMAIL = FROM_EMAIL
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = "smtp.sendgrid.net"
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = "apikey"        # always "apikey"
+# EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+# DEFAULT_FROM_EMAIL = FROM_EMAIL
+
+
+EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
+
+# Optional configs
+SENDGRID_SANDBOX_MODE_IN_DEBUG = False
+SENDGRID_ECHO_TO_STDOUT = False
+
+DEFAULT_FROM_EMAIL = "teerdavenigedela@gmail.com"
+    
